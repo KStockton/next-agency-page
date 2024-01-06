b1c6c3be312313151096af8355ac9ff6c2c951c5
kingstockton@gmail.com <>
2024-01-06 16:31:03 -0700

    Add homepage.

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import Image from "next/image";
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
-}
+    <div>
+      <Image src="/about.png" alt="about" width={500} height={500} />
+    </div>
+  );
+};
 
-export default AboutPage
+export default AboutPage;
